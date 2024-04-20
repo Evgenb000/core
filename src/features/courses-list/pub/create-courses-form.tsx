18d9165870a826a30cc87ccf8ac01fb34f2f0d87
Eugene <ebabin000@gmail.com>
2024-04-20 12:29:56 +0300

    jest, jest config

--- src/features/courses-list/pub/create-courses-form.tsx
+++ src/features/courses-list/pub/create-courses-form.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React, { startTransition } from "react";
+import React from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { createCourseAction } from "../action";
